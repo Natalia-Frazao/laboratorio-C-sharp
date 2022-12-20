5427bec0d66bb6b6e6aa2a662491cb3011fdf84e
Natália Frazão <110553454+Natalia-Frazao@users.noreply.github.com>
2022-12-20 20:48:11 -0300

    commit inicial

---
+++ Semana 6-Wiz/NotaFiscal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+public class NotaFiscal
+{
+    public NotaFiscal()
+    {
+    }
+
+    public String nomeCliente { get; set; }
+
+    public  DateTime dataCompra { get; set; }
+
+    public float total { get; set; }
+
+
+    protected String Pedido
+    {
+        get
+        {
+            return nomeCliente + " " + dataCompra + " " + total;
+
+        }
+    }
+        
+    
+}
